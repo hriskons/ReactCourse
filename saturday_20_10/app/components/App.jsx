65fa5f499eb5c9f2f9de913f5a504d4fdc347369
Konstantas <christos.konstantas@accenture.com>
2018-10-27 13:04:57 +0300

    Adding example of contacts fetching

--- saturday_20_10/app/components/App.jsx
+++ saturday_20_10/app/components/App.jsx
@@ -1,15 +1,11 @@
 import React from "react";
-import Box from "./Box"
-import Hero from "./Hero"
 import "react-bootstrap"
+import DataFetching from "./DataFetching";
 
 const App = (props) => {
     return (
         <div>
-            <button>{props.buttonName}</button>
-            <h1>{props.title}</h1>
-            <Hero heroName = {"Superman"}></Hero>
-            <Box boxType = {"Red Box"}></Box>
+            <DataFetching />
         </div>
     )
 }
