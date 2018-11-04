3b4e855885ae0a30b997839b7c70568860dffe47
johnsk50 <giannis.skoulas.ch@gmail.com>
2018-11-04 09:34:06 +0200

    Dashboard + css changes

--- class_project/app/components/Dashboard.jsx
+++ class_project/app/components/Dashboard.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-import {Col, Badge, Row, Jumbotron, Panel} from "react-bootstrap";
+import {Col, Badge, Row, Jumbotron, Panel, Table, Button} from "react-bootstrap";
 
-const coursesAPI = 'http://localhost:3000/stats';
-
+const statsAPI = 'http://localhost:3000/stats';
+const coursesAPI = 'http://localhost:3000/courses';
 
 class Dashboard extends React.Component {
 
@@ -12,32 +12,66 @@ class Dashboard extends React.Component {
         this.state = {
           error: null,
           isLoaded: false,
-          items: []
+          statsItems: [],
+          coursesItems: []
+
         }; 
       }
 
       componentDidMount = () => {
-        fetch(coursesAPI)
+        fetch(statsAPI)
         .then(function(response) {
             return response.json();
-        }).then((myJson) => {
-            console.log(myJson);
+        }).then((stats) => {
+            this.setState({
+                statsItems: stats
+            });
+            return fetch(coursesAPI);
+        }).then(function(response){
+            return response.json();
+        }).then((courses)=>{
             this.setState({
                 isLoaded: true,
-                items: myJson
+                coursesItems: courses
             });
-        });
+        });  
+        
     }
 
+     convertDate(stringdate)
+        {
+            // Internet Explorer does not like dashes in dates when converting, 
+            // so lets use a regular expression to get the year, month, and day 
+            var DateRegex = /([^-]*)-([^-]*)-([^-]*)/;
+            var DateRegexResult = stringdate.match(DateRegex);
+            var DateResult;
+            var StringDateResult = "";
+
+            // try creating a new date in a format that both Firefox and Internet Explorer understand
+            try
+            {
+                DateResult = new Date(DateRegexResult[2]+"/"+DateRegexResult[3]+"/"+DateRegexResult[1]);
+            } 
+            // if there is an error, catch it and try to set the date result using a simple conversion
+            catch(err) 
+            { 
+                DateResult = new Date(stringdate);
+            }
+
+            // format the date properly for viewing
+            StringDateResult = (DateResult.getMonth()+1)+"/"+(DateResult.getDate()+1)+"/"+(DateResult.getFullYear());
+
+            return StringDateResult;
+        }
+
     render() {
-        const { error, isLoaded, items } = this.state;
+        const { error, isLoaded, statsItems,coursesItems  } = this.state;
         if (error) {
             return <div>Error: {error.message}</div>;
         } else if (!isLoaded) {
             return <div className="container" style={{marginTop: "50px", maxWidth: "1000px"}}>Loading...</div>;
         } else {
-            return (
-                
+            return (              
 
                 <div>
                     <div className="container">
@@ -48,58 +82,61 @@ class Dashboard extends React.Component {
                     </div>
                     <div className="container">
                         <Row>
-                            {items.map(item => (
+                            {statsItems.map(stat => (
                                 
-                                    <Col xs={12} sm={3}  key={item.id}>
-                                        <Panel >
-                                    <Panel.Body>
-                                            <Col xs={8} sm={10} style={{alignContent: screenLeft}}>
-                                            <p>{item.title.toUpperCase()}:</p>
-                                            </Col>
-                                            <Col xs={4} sm={2} >
-                                            <Badge>{item.amount} </Badge>
-                                            </Col>
-                                        
-                                            </Panel.Body>
-                                </Panel>
-                                    
-                                    </Col>
-                                    
-                                // <li key={item.id}>
-                                //     {item.id} {item.title} {item.amount} 
-                                // </li>
+                                    <Col xs={12} sm={3}  key={stat.id}>
+                                        <Panel className="dashboard-panel">
+                                            <Panel.Body>
+                                                <Col xs={8} sm={8}>
+                                                    <h5>{stat.title.toUpperCase()}:</h5>
+                                                </Col>
+                                                <Col xs={4} sm={4} >
+                                                    <Badge>{stat.amount} </Badge>
+                                                </Col>   
+                                            </Panel.Body>                                     
+                                        </Panel>                                   
+                                    </Col>                                    
                             ))}
+                            
                         </Row>
+                        <div className="table-container">
+                            <div className="table-header">
+                            <h3>Last 5 Courses</h3>
+                            </div>
+                            <Table striped bordered condensed hover>
+                                <thead>
+                                    <tr>
+                                    <th></th>
+                                    <th>Title</th>
+                                    <th>Bookable</th>
+                                    <th>Price</th>
+                                    <th>Date</th>
+                                    <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                {coursesItems.map(course => (
+                                    <tr key={course.id}>
+                                    <td>&#8505;</td>
+                                    <td>{course.title}</td>
+                                    {course.open && <td>&#10004;</td>}
+                                    {!course.open && <td>&#10006;</td>}
+                                    <td>{course.price.normal}&nbsp;{'\u20AC'}</td>
+                                    <td>{this.convertDate(course.dates.start_date)}&nbsp;-&nbsp;{this.convertDate(course.dates.end_date)}</td>
+                                    <td style={{textAlign: 'right'}}><Button bsStyle="info" bsSize="xsmall">View details</Button></td>
+                                    </tr>
+                                ))}
+                                </tbody>
+                            </Table>
+                            <div className="table-footer">
+                                <Button>View All</Button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             );
         }
     }
-
-
-}
-
-
-
-// const Dashboard = (props) => {
-//     return (
-//         <div>
-//         </div>
-//     )
-// }
-
-const CourseTable = (props) => {
-    return (
-        <div>
-        </div>
-    )
-}
-
-const StatItem = (props) => {
-    return (
-        <div>
-        </div>
-    )
 }
 
 export default Dashboard;
