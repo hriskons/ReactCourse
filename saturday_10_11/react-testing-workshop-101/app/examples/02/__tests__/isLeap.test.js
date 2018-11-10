635f5089cadf11ca6ebc8a4248ee59e941395a81
Christos Konstantas <ckonstantas@gmail.com>
2018-11-10 11:27:04 +0200

    Adding solution to leap year

--- saturday_10_11/react-testing-workshop-101/app/examples/02/__tests__/isLeap.test.js
+++ saturday_10_11/react-testing-workshop-101/app/examples/02/__tests__/isLeap.test.js
@@ -5,19 +5,19 @@ describe("A leap year", () => {
     expect(isLeap(2015)).toBeFalsy();
   });
 
-  xit("year divisible by 4, not divisible by 100: leap year", () => {
+  it("year divisible by 4, not divisible by 100: leap year", () => {
     expect(isLeap(2016)).toBeTruthy();
   });
 
-  xit("year divisible by 100, not divisible by 400: common year", () => {
+  it("year divisible by 100, not divisible by 400: common year", () => {
     expect(isLeap(2100)).toBeFalsy();
   });
 
-  xit("year divisible by 400: leap year", () => {
+  it("year divisible by 400: leap year", () => {
     expect(isLeap(2000)).toBeTruthy();
   });
 
-  xit("year divisible by 200, not divisible by 400: common year", () => {
+  it("year divisible by 200, not divisible by 400: common year", () => {
     expect(isLeap(1800)).toBeFalsy();
   });
 });
