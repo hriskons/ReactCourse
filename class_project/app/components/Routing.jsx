34042f4dbc68d4928b45f2e9c759f2ff6b6c135d
Christos Konstantas <ckonstantas@gmail.com>
2018-11-10 16:53:28 +0200

    Cleaning up auxiliary

--- class_project/app/components/Routing.jsx
+++ class_project/app/components/Routing.jsx
@@ -1,5 +1,5 @@
 import React  from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 import { Switch } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Courses from "./Courses";
